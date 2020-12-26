0375d670fc1616e92379253e6b6c44a428ef9259
Wojciech Powroźnik <60233834+WojciechPowroznik@users.noreply.github.com>
2020-12-26 17:06:48 +0100

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,28 @@
+const {Builder, By, until} = require("selenium-webdriver");
+
+(async function example(){
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://lamp.ii.us.edu.pl/~mtdyd/zawody/");
+    await driver.findElement(By.id("inputEmail3")).click();
+    await driver.findElement(By.id("inputEmail3")).sendKeys('adam');
+    await driver.findElement(By.id("inputPassword3")).click();
+    await driver.findElement(By.id("inputPassword3")).sendKeys('kowalski');
+    await driver.findElement(By.id("dataU")).click();
+    await driver.findElement(By.id("dataU")).sendKeys('01-01-2010 ');
+    await driver.findElement(By.xpath("//button[@type='button']")).click();
+    await driver.wait(until.alertIsPresent());
+    let alert1 = await driver.switchTo().alert();
+    await alert1.accept();
+    await driver.wait(until.alertIsPresent());
+    let alert2 = await driver.switchTo().alert();
+    let alertText = await alert2.getText();
+    await alert2.accept();
+    if(alertText == "Blad danych"){
+        console.log("Passed");
+    } else {
+        console.log("Failed");
+    }
+    driver.quit();
+})();
+
+
